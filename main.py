b18218ee7973a6e4db1d5d3d9ff9203c5d61155f
Andrea <andrea.masi@estudiantat.upc.edu>
2020-12-06 14:27:49 +0100

    Preprocessing re-updated

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from lazylearning import knn
 
 if __name__ == '__main__':
 
-    # train_matrix, train_labels, test_matrix, test_labels = dr.process_dataset('numerical', 7)
-    # print(train_matrix.shape, test_matrix.shape)
+    #train_matrix, train_labels, test_matrix, test_labels = dr.read_processed_data('numerical', 7, force_creation=False)
+    #print(train_matrix.shape, test_matrix.shape)
 
-    train_matrix, train_labels, test_matrix, test_labels = dr.process_dataset('mixed', 7)
+    train_matrix, train_labels, test_matrix, test_labels = dr.read_processed_data('mixed', 7, force_creation=True)
     print(train_matrix.shape, test_matrix.shape)
-    # train_matrices, train_matrices_labels,  test_matrices, test_matrices_labels = dr.get_ten_fold_preprocessed_dataset('numerical')
-    # datasets_preprocessed = dr.get_datasets()
+    # train_matrices, train_matrices_labels,  test_matrices, test_matrices_labels = dr.get_ten_fold_preprocessed_dataset('numerical', force_creation=False)
+    # datasets_preprocessed = dr.get_datasets(force_creation=False)
 
     predictions = knn.kNNAlgorithm(train_matrix, train_labels, test_matrix,
-                                   k=1, distance='euclidean', policy='majority', weights=None)
+                                  k=1, distance='euclidean', policy='majority', weights=None)
 
     """kNNAlgorithm.fit(train_matrix, train_labels)
     predictions = kNNAlgorithm.predict(test_matrix)
-    accuracy = kNNAlgorithm.evaluate(test_labels, predictions)"""
+    accuracy = kNNAlgorithm.evaluate(test_labels, predictions)
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+"""
 
--- src/arffdatasetreader/dataset_reader.py
+++ src/arffdatasetreader/dataset_reader.py
@@ -7,89 +7,137 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 
 
-def process_dataset(dataset_type: str, fold_index: int) -> (np.ndarray, np.ndarray):
+def read_processed_data(dataset_type: str, fold_index: int, force_creation: bool = False) -> (np.ndarray, np.ndarray,
+                                                                                      np.ndarray, np.ndarray):
+
     accepted_ds_types = ['numerical', 'mixed']
     if dataset_type not in accepted_ds_types:
         raise ValueError(f"{dataset_type}:: not valid dataset type")
+
     if fold_index not in range(0, 10):
         raise ValueError(f"{dataset_type}:: not valid fold index")
 
-    print("Reading dataset: " + dataset_type)
+    processed = "_processed"
+    csv = ".csv"
+    arff = ".arff"
+    train_fold = f".fold.00000{fold_index}.train"
+    test_fold = f".fold.00000{fold_index}.test"
+
+    print(f"For {dataset_type} dataset train and test fold n°{fold_index}.")
     if dataset_type == 'numerical':
-        num_ds_path = "datasets/pen-based/pen-based"
-        return process_num_data(num_ds_path, fold_index)
+        num_ds_path = "datasets/pen-based"
+        num_ds_path_processed = num_ds_path + "/processed/pen-based"
+        if not force_creation:
+            split_dataset = read_from_csv(num_ds_path_processed + train_fold + processed + csv,
+                                          num_ds_path_processed + test_fold + processed + csv)
+            if split_dataset is None:
+                process_num_data(num_ds_path + "/pen-based" + train_fold + arff, num_ds_path + "/pen-based" + test_fold + arff)
+                split_dataset = read_from_csv(num_ds_path_processed + train_fold + processed + csv,
+                                          num_ds_path_processed + test_fold + processed + csv)
+            return split_dataset[0], split_dataset[1], split_dataset[2], split_dataset[3]
+
+        else:
+            process_num_data(num_ds_path + "/pen-based" + train_fold + arff, num_ds_path + "/pen-based" + test_fold + arff)
+            split_dataset = read_from_csv(num_ds_path_processed + train_fold + processed + csv,
+                                          num_ds_path_processed + test_fold + processed + csv)
+            return split_dataset[0], split_dataset[1], split_dataset[2], split_dataset[3]
 
     if dataset_type == 'mixed':
-        mix_ds_path = "datasets/hypothyroid/hypothyroid"
-        return process_mix_data(mix_ds_path, fold_index)
+        mix_ds_path = "datasets/hypothyroid"
+        mix_ds_path_processed = mix_ds_path + "/processed/hypothyroid"
+        if not force_creation:
+            split_dataset = read_from_csv(mix_ds_path_processed + train_fold + processed + csv,
+                                          mix_ds_path_processed + test_fold + processed + csv)
+            if split_dataset is None:
+                process_mix_data(mix_ds_path + "/hypothyroid" + train_fold + arff, mix_ds_path + "/hypothyroid" + test_fold + arff)
+                split_dataset = read_from_csv(mix_ds_path_processed + train_fold + processed + csv,
+                                          mix_ds_path_processed + test_fold + processed + csv)
+            return split_dataset[0], split_dataset[1], split_dataset[2], split_dataset[3]
+
+        else:
+            process_mix_data(mix_ds_path + "/hypothyroid" + train_fold + arff, mix_ds_path + "/hypothyroid" + test_fold + arff)
+            split_dataset = read_from_csv(mix_ds_path_processed + train_fold + processed + csv,
+                                          mix_ds_path_processed + test_fold + processed + csv)
+            return split_dataset[0], split_dataset[1], split_dataset[2], split_dataset[3]
 
 
-def process_num_data(path: str, fold_index: int) -> (np.ndarray, np.ndarray):
-    print(f"Processing Numerical Train and Test fold n°{fold_index}...")
+def process_num_data(path_train: str, path_test: str):
+    print(f"Processing Numerical train and test fold...")
 
-    numerical_train_df, numerical_test_df = from_arff_to_pandas_dataframe(fold_index, path)
+    numerical_train_df, numerical_test_df = from_arff_to_pandas_dataframe(path_train, path_test)
     apply_decoding(numerical_train_df)
     apply_decoding(numerical_test_df)
 
-    # Saving train class column
-    train_real_labels = numerical_train_df["a17"]
-    numerical_train_df.drop(numerical_train_df.iloc[:, -1:], axis=1, inplace=True)
-
-    # Saving test class column
-    test_real_labels = numerical_test_df["a17"]
-    numerical_test_df.drop(numerical_test_df.iloc[:, -1:], axis=1, inplace=True)
-
+    create_csv_files(numerical_train_df, numerical_test_df, path_train, path_test)
     print("Numerical matrices created.")
-    return numerical_train_df.to_numpy(), train_real_labels.to_numpy(), numerical_test_df.to_numpy(), test_real_labels.to_numpy()
 
 
-def process_mix_data(path: str, fold_index: int) -> (np.ndarray, np.ndarray):
-    print(f"Processing Mixed Train and Test fold n°{fold_index}...")
+def process_mix_data(path_train: str, path_test: str):
+    print(f"Processing Mixed train and test fold...")
 
-    mixed_train_df, mixed_test_df = from_arff_to_pandas_dataframe(fold_index, path)
+    mixed_train_df, mixed_test_df = from_arff_to_pandas_dataframe(path_train, path_test)
 
     apply_decoding(mixed_train_df)
     apply_decoding(mixed_test_df)
 
     # Saving train class column
-    train_real_labels = mixed_train_df["Class"]
+    train_real_labels = mixed_train_df['Class']
     mixed_train_df.drop(mixed_train_df.iloc[:, -1:], axis=1, inplace=True)
 
+    # Saving train last row
+    last_index_train = mixed_train_df.shape[0]
+
     # Saving test class column
-    test_real_labels = mixed_test_df["Class"]
+    test_real_labels = mixed_test_df['Class']
     mixed_test_df.drop(mixed_test_df.iloc[:, -1:], axis=1, inplace=True)
 
-    mixed_train_cleaned = dealing_with_missing_values(mixed_train_df)
-    mixed_test_cleaned = dealing_with_missing_values(mixed_test_df)
+    mixed_full_dataset = pd.concat([mixed_train_df, mixed_test_df])
+    mixed_full_dataset_cleaned = dealing_with_missing_values(mixed_full_dataset)
+    apply_label_encoding(mixed_full_dataset_cleaned)
+    mixed_full_dataset_encoded = pd.get_dummies(mixed_full_dataset_cleaned)
+    mixed_full_dataset_normalized = apply_normalization(mixed_full_dataset_encoded)
 
-    # Label encoding for binary columns
-    columns_label_encoding = ['sex', 'on_thyroxine', 'query_on_thyroxine',
-                              'on_antithyroid_medication', 'sick', 'pregnant', 'thyroid_surgery',
-                              'I131_treatment', 'query_hypothyroid', 'query_hyperthyroid', 'lithium',
-                              'goitre', 'tumor', 'hypopituitary', 'psych', 'TSH_measured',
-                              'T3_measured', 'TT4_measured', 'T4U_measured',
-                              'FTI_measured']
+    processed_train = mixed_full_dataset_normalized[0:last_index_train]
+    processed_test = mixed_full_dataset_normalized[last_index_train:].reset_index(drop=True)
+    processed_train_with_class = pd.concat([processed_train, train_real_labels], axis=1)
+    processed_test_with_class = pd.concat([processed_test, test_real_labels], axis=1)
 
-    apply_label_encoding(mixed_train_cleaned, columns_label_encoding)
-    apply_label_encoding(mixed_test_cleaned, columns_label_encoding)
-
-    # One hot encoding for the last one 'referral source'
-    mixed_train_encoded = pd.get_dummies(mixed_train_cleaned)
-    mixed_test_encoded = pd.get_dummies(mixed_test_cleaned)
-
-    mixed_train_normalized = apply_normalization(mixed_train_encoded)
-    mixed_test_normalized = apply_normalization(mixed_test_encoded)
-
+    create_csv_files(processed_train_with_class, processed_test_with_class, path_train, path_test)
     print("Mixed matrices created.")
-    return mixed_train_normalized.to_numpy(), train_real_labels.to_numpy(), mixed_test_normalized.to_numpy(), test_real_labels.to_numpy()
 
 
-def from_arff_to_pandas_dataframe(fold_index, path):
-    train_dataset, train_meta = arff.loadarff(f"{path}.fold.00000{fold_index}.train.arff")
-    test_dataset, test_meta = arff.loadarff(f"{path}.fold.00000{fold_index}.test.arff")
+def from_arff_to_pandas_dataframe(path_train, path_test):
+    train_dataset, train_meta = arff.loadarff(path_train)
+    test_dataset, test_meta = arff.loadarff(path_test)
     train_df = pd.DataFrame(train_dataset)
     test_df = pd.DataFrame(test_dataset)
     return train_df, test_df
+
+
+def read_from_csv(path_train: str, path_test: str) -> np.ndarray:
+    print("Reading from csv file...")
+    try:
+        train_dataset = pd.read_csv(path_train)
+        test_dataset = pd.read_csv(path_test)
+        train_labels = train_dataset.iloc[:, -1:]
+        test_labels = test_dataset.iloc[:, -1:]
+        train_dataset.drop(train_dataset.iloc[:, -1:], axis=1, inplace=True)
+        test_dataset.drop(test_dataset.iloc[:, -1:], axis=1, inplace=True)
+        result = [train_dataset.to_numpy(), train_labels.to_numpy(), test_dataset.to_numpy(), test_labels.to_numpy()]
+        return result
+    except FileNotFoundError as e:
+        print("Processed dataset train test files not found.")
+        return None
+
+
+def create_csv_files(train_df, test_df, path_train: str, path_test: str):
+    path_train_parts = path_train.split("/")
+    path_test_parts = path_test.split("/")
+    file_name_train = path_train_parts[2][:-5]
+    file_name_test = path_test_parts[2][:-5]
+    path_train_processed = path_train_parts[0] + "/" + path_train_parts[1] + "/processed/"
+    train_df.to_csv(path_train_processed + file_name_train + "_processed.csv", index=False)
+    test_df.to_csv(path_train_processed + file_name_test + "_processed.csv", index=False)
 
 
 def apply_normalization(pandas_dataframe):
@@ -99,7 +147,14 @@ def apply_normalization(pandas_dataframe):
     return pd.DataFrame(values_normalized, columns=pandas_dataframe.columns)
 
 
-def apply_label_encoding(pandas_dataframe, columns):
+def apply_label_encoding(pandas_dataframe):
+    # Label encoding for binary columns
+    columns = ['sex', 'on_thyroxine', 'query_on_thyroxine',
+                              'on_antithyroid_medication', 'sick', 'pregnant', 'thyroid_surgery',
+                              'I131_treatment', 'query_hypothyroid', 'query_hyperthyroid', 'lithium',
+                              'goitre', 'tumor', 'hypopituitary', 'psych', 'TSH_measured',
+                              'T3_measured', 'TT4_measured', 'T4U_measured',
+                              'FTI_measured']
     # Label Encoding
     for column in columns:
         le = LabelEncoder()
@@ -141,7 +196,8 @@ def print_count_values_per_column(df: pd.DataFrame, columns: list, show_descript
             print("******************")
 
 
-def get_ten_fold_preprocessed_dataset(dataset_type: str) -> (np.ndarray, np.ndarray):
+def get_ten_fold_preprocessed_dataset(dataset_type: str, force_creation: bool = False) -> (np.ndarray, np.ndarray,
+                                                                                      np.ndarray, np.ndarray):
     accepted_ds_types = ['numerical', 'mixed']
     if dataset_type not in accepted_ds_types:
         raise ValueError(f"{dataset_type}:: not valid dataset type")
@@ -150,7 +206,7 @@ def get_ten_fold_preprocessed_dataset(dataset_type: str) -> (np.ndarray, np.ndar
     test_matrices = []
     test_matrices_labels = []
     for fold_index in range(0, 10):
-        train_matrix, train_matrix_labels, test_matrix, test_matrix_labels = process_dataset(dataset_type, fold_index)
+        train_matrix, train_matrix_labels, test_matrix, test_matrix_labels = read_processed_data(dataset_type, fold_index, force_creation)
 
         train_matrices.append(train_matrix)
         train_matrices_labels.append(train_matrix_labels)
@@ -161,8 +217,8 @@ def get_ten_fold_preprocessed_dataset(dataset_type: str) -> (np.ndarray, np.ndar
     return train_matrices, train_matrices_labels,  test_matrices, test_matrices_labels
 
 
-def get_datasets() -> np.ndarray:
-    num_train_matrices, num_train_matrices_labels, num_test_matrices, num_test_matrices_labels = get_ten_fold_preprocessed_dataset(dataset_type='numerical')
-    mix_train_matrices, mix_train_matrices_labels, mix_test_matrices, mix_test_matrices_labels = get_ten_fold_preprocessed_dataset(dataset_type='mixed')
+def get_datasets(force_creation: bool = False) -> np.ndarray:
+    num_train_matrices, num_train_matrices_labels, num_test_matrices, num_test_matrices_labels = get_ten_fold_preprocessed_dataset(dataset_type='numerical', force_creation=force_creation)
+    mix_train_matrices, mix_train_matrices_labels, mix_test_matrices, mix_test_matrices_labels = get_ten_fold_preprocessed_dataset(dataset_type='mixed', force_creation=force_creation)
     return [(num_train_matrices, num_train_matrices_labels, num_test_matrices, num_test_matrices_labels),
             (mix_train_matrices, mix_train_matrices_labels, mix_test_matrices, mix_test_matrices_labels)]

dc4532a86cbd300e462d69ba5782f7f95203e1b3
albaporras <66181309+albaporras@users.noreply.github.com>
2020-12-08 10:11:55 +0100

    Update distances.py

--- src/utils/distances.py
+++ src/utils/distances.py
@@ -11,13 +11,14 @@ def euclidean_distances(X, Y, weights=None):
 
 
 def manhattan_distances(X, Y, weights=None):
-    # TODO : Put the computation of the distances in a function as it was done in euclidean distances. Let
-    #  'weights' be a possible parameter.
-    # distances = [[np.abs(np.subtract(x, y)).sum() for y in train_matrix] for x in test_matrix]
-    return None
+    if weights is None:
+        weights = np.ones(X.shape[1])
+        
+    return [[np.multiply(weights, np.abs(np.subtract(x, y))).sum() for y in X] for x in Y]
 
 
 def chebychev_distances(X, Y, weights=None):
-    # TODO : Put the computation of the distances in a function as it was done in euclidean distances. Let
-    #  'weights' be a possible parameter.
-    distances = [[max(np.abs(np.subtract(x, y))) for y in train_matrix] for x in test_matrix]
+    if weights is None:
+        weights = np.ones(X.shape[1])
+
+    return [[max(np.multiply(weights, np.abs(np.subtract(x, y)))) for y in X] for x in Y]

36567cad91ad3a82e36b51c1c2164d853b1d2533
kevinrosalesdev <kevinrosalesdev@gmail.com>
2020-12-13 12:40:11 +0100

    reduction techniques template

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from arffdatasetreader import dataset_reader as dr
 from evaluation import evaluation
 from utils import weighting, plotter
 from lazylearning.KnnAlgorithm import KnnAlgorithm
+from lazylearning.ReductionKnnAlgorithm import ReductionKnnAlgorithm
 import numpy as np
 
 if __name__ == '__main__':
@@ -21,12 +22,18 @@ if __name__ == '__main__':
     # print("ReliefF weights:\n" + str(weighting.get_relieff_weights(dataset, labels)))
     # print("IG weights:\n" + str(weighting.get_ig_weights(dataset, labels)))
 
-    print("Majority policy on numerical dataset")
-    knn = KnnAlgorithm(k=7, distance='euclidean', policy='majority', weights=None, verbosity=False)
-    knn.fit(train_matrix=train_matrix, train_labels=train_matrix_labels)
-    predictions = knn.predict(test_matrix)
-    accuracy, execution_time = knn.evaluate(test_matrix_labels, predictions)
-    print(accuracy, execution_time)
+    reductionKnn = ReductionKnnAlgorithm(k=7, distance='euclidean', policy='majority', weights=None, verbosity=False)
+    reductionKnn.fit(train_matrix=train_matrix, train_labels=train_matrix_labels, reduction_technique='X')
+    # predictions = reductionKnn.predict(test_matrix)
+    # accuracy, execution_time = reductionKnn.evaluate(test_matrix_labels, predictions)
+    # print(accuracy, execution_time)
+
+    # print("Majority policy on numerical dataset")
+    # knn = KnnAlgorithm(k=7, distance='euclidean', policy='majority', weights=None, verbosity=False)
+    # knn.fit(train_matrix=train_matrix, train_labels=train_matrix_labels)
+    # predictions = knn.predict(test_matrix)
+    # accuracy, execution_time = knn.evaluate(test_matrix_labels, predictions)
+    # print(accuracy, execution_time)
 
     # evaluation.evaluate_knn('mixed')
     # average_accuracy_list = [0.8586822566933352, 0.9143757804897203, 0.77757804897203, 0.9743757804897203]

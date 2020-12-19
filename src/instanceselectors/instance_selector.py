9e371d7c2228c2237de5a6773ca7f100dcdae54b
Andrea <andrea.masi@estudiantat.upc.edu>
2020-12-19 19:05:17 +0100

    bugs fixed.

--- src/instanceselectors/instance_selector.py
+++ src/instanceselectors/instance_selector.py
@@ -6,16 +6,12 @@ import pandas as pd
 
 
 def snn(train_matrix, train_labels):
+    print("Starting SNN reduction...")
     tic = time.time()
     distances = dt.euclidean_distances(train_matrix, train_matrix, None)
     ordered_matrix = np.argsort(distances, axis=1)
+    print("argsort exec. time=", time.time()-tic)
 
-    """    
-    tic = time.time()
-    nearest_enemy = [next((nn for nn in instance if train_labels[nn] != train_labels[instance[0]]), None) for instance in ordered_matrix]
-    print(time.time()-tic)
-    """
-    print(time.time()-tic)
     tic = time.time()
     nearest_enemy = []
     for row in ordered_matrix:
@@ -24,37 +20,42 @@ def snn(train_matrix, train_labels):
             if train_labels[row[0]] != train_labels[nn]:
                 nearest_enemy.append(nn)
                 break
-    print(time.time()-tic)
+    print("N. enemy exec. time=", time.time()-tic)
 
-    binary_matrix_numpy = np.zeros((len(train_matrix), len(train_matrix)))
-    for i in range(len(binary_matrix_numpy)):
-        for j in range(len(binary_matrix_numpy)):
+    tic = time.time()
+    binary_matrix_numpy = np.zeros((len(train_matrix), len(train_matrix)), dtype='i1')
+    for i in range(binary_matrix_numpy.shape[1]):
+        for j in range(binary_matrix_numpy.shape[1]):
             if train_labels[j] == train_labels[i] and distances[i, j] <= distances[i, nearest_enemy[i]]:
                 binary_matrix_numpy[i, j] = 1
-    print(time.time()-tic)
 
-    binary_matrix_numpy = np.array([
-        [1,0,0,0,0,1,0,0],
-        [0,1,0,0,0,0,1,0],
-        [0,0,1,0,0,0,0,0],
-        [0,1,0,1,1,0,0,0],
-        [0,0,0,0,1,0,1,0],
-        [0,0,1,0,0,1,0,0],
-        [0,0,0,1,0,0,1,0],
-        [0,0,0,1,0,0,0,1],
-    ])
-    train_matrix = np.array([[1],[2],[3],[4],[5],[6],[7],[8]])
+    # binary_matrix_numpy = np.array([
+    #     [1,0,0,0,0,1,0,0],
+    #     [0,1,0,0,0,0,1,0],
+    #     [0,0,1,0,0,0,0,0],
+    #     [0,1,0,1,1,0,0,0],
+    #     [0,0,0,0,1,0,1,0],
+    #     [0,0,1,0,0,1,0,0],
+    #     [0,0,0,1,0,0,1,0],
+    #     [0,0,0,1,0,0,0,1],
+    # ])
+    # train_matrix = np.array([[1],[2],[3],[4],[5],[6],[7],[8]])
+
     binary_matrix = pd.DataFrame(binary_matrix_numpy)
+    print("Binary matrix exec. time=", time.time()-tic)
 
     selected_train_rows = []
     binary_matrix, _ = main_reduction(binary_matrix, selected_train_rows, num_samples_needed=None)
     if binary_matrix.shape[1] > 0:
         recursive_reduction(binary_matrix, selected_train_rows)
 
-    reduced_train = [train_matrix[row_index] for row_index in selected_train_rows]
+    print("finalizing...")
+    reduced_train = np.array([train_matrix[row_index] for row_index in selected_train_rows])
     return reduced_train
 
+
 def main_reduction(binary_matrix, selected_train_rows, num_samples_needed=None):
+    print("main reduction...")
     converged = False
     if num_samples_needed is None:
         num_samples_needed = float('inf')
@@ -83,12 +84,9 @@ def select_unitary_rows(binary_matrix, selected_train_rows):
     selected_rows_index = selected_rows.index
     if len(selected_rows_index) > 0:
         # drop rows where columns sum up to 1
-        # reduced_binary_matrix = binary_matrix[~(binary_matrix.sum(axis=1) == 1)]
         reduced_binary_matrix = binary_matrix.drop(selected_rows_index, axis=0)
         # drop columns where dropped rows had value of 1
         reduced_binary_matrix = reduced_binary_matrix.loc[:, selected_rows.sum(axis=0) == 0]
-        # drop columns with only zeros (relative to the dropped rows with just one bit)
-        reduced_binary_matrix = reduced_binary_matrix.loc[:, (reduced_binary_matrix != 0).any(axis=0)]
         # double check if some columns are now with just one bit
         binary_matrix = reduced_binary_matrix.loc[:, reduced_binary_matrix.sum(axis=0) > 1]
         selected_train_rows.extend(selected_rows_index)
@@ -100,10 +98,11 @@ def select_unitary_rows(binary_matrix, selected_train_rows):
 def drop_minor_rows(binary_matrix: pd.DataFrame):
     """comparing two rows at the time deleting the one with all minor or equals values then the other"""
     no_changes = True
-    for idx in binary_matrix.index:
+    rows_index = binary_matrix.index
+    for idx in rows_index:
         curr_row = binary_matrix.loc[idx]
         curr_matrix = binary_matrix.drop(idx, axis=0)
-        if (curr_matrix >= curr_row).all(axis=1).any():
+        if curr_matrix.ge(curr_row, axis=1).all(axis=1).any():
             binary_matrix = curr_matrix
             no_changes = False
     return binary_matrix, no_changes
@@ -115,8 +114,7 @@ def drop_major_columns(binary_matrix: pd.DataFrame):
     for label in binary_matrix.columns:
         curr_col = binary_matrix[label]
         curr_matrix = binary_matrix.drop(label, axis=1)
-        print((curr_matrix <= curr_col).all(axis=1).any())
-        if (curr_matrix <= curr_col).all(axis=1).any():
+        if curr_matrix.le(curr_col, axis=0).all(0).any():
             binary_matrix = curr_matrix
             no_changes = False
     return binary_matrix, no_changes
@@ -124,6 +122,7 @@ def drop_major_columns(binary_matrix: pd.DataFrame):
 
 def recursive_reduction(binary_matrix, selected_train_rows):
     """search for the next sample that should be placed into the selective subset"""
+    print("starting recursive reduction...")
     num_remaining_columns = binary_matrix.shape[1]
     results_dict = dict()
     sorted_sums = binary_matrix.sum(axis=1).sort_values(ascending=False)
@@ -134,7 +133,6 @@ def recursive_reduction(binary_matrix, selected_train_rows):
     for absolute_min in set(results_dict.values()):
         selected_labels = [k for k, v in results_dict.items() if v == absolute_min]
         for row_label in selected_labels:
-            print(absolute_min, row_label)
             # remove selected row and columns with 1 bits in the removed row
             temp_binary_matrix = binary_matrix.copy()
             subset_row = temp_binary_matrix.loc[row_label]
